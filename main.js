d6e1496271a6c22027222059b3f02374563d3212
saidmuhammad03 <valisajmuhammad@gmail.com>
2023-11-23 02:24:02 -0800

    kfgfvkj

---
+++ main.js
@@ -0,0 +1,30 @@
+//task 1
+
+// const get=(nam)=>` Hello ${nam}`
+// console.log(get("sad"));
+
+//task2
+// const get=(harf)=>{
+//     if(harf.includes("salom")) {
+//         return true }
+//     else{
+//      return false }
+// }
+// console.log(get("world"));
+
+//task4
+// function get(str){
+     
+//        let result= str.split("-")
+//        return result.length
+// }
+
+
+// console.log(get("man-kalon-meshavam-dar"));
+//task5
+   function get(n) {   
+    
+   }
+
+
+
